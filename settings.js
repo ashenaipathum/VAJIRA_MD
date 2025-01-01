f30b5a427bd596184cb92b8e90cd4b3980318536
pathum(ashen ) <137178189+ashenaipathum@users.noreply.github.com>
2025-01-02 01:43:20 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,7 +10,7 @@ SESSION_ID: process.env.SESSION_ID === undefined ? 'Put your session id here' : 
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '' : process.env.OWNER_NUMBER,
 N_JID: process.env.N_JID=== undefined ? '‌': process.env.N_JID,    
 PREFIX: process.env.PREFIX || '.' ,
-FOOTER: process.env.FOOTER=== undefined ? '‌👨‍💻 ᴠᴀᴊɪʀᴀ ᴍᴅ ʙʏ ᴛᴄ ᴛᴇᴀᴍ 👨‍💻': process.env.FOOTER,
+FOOTER: process.env.FOOTER=== undefined ? '‌👨‍💻 ඕක බදින්න වෙන්නෑ....!!!! 👨‍💻': process.env.FOOTER,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgres://vajiratech_user:oSIFl2xmSojMZ0rkzdd0g0W6msuVTpNN@dpg-cpd7fjv109ks73e5gtig-a.frankfurt-postgres.render.com/vajiratech' : process.env.POSTGRESQL_URL,   
 MAX_SIZE: 500,
 PAIR: 'https://vajirasessionid-78fa4ff8a63a.herokuapp.com/code?number=',       
